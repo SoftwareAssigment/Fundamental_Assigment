40a9d2f4afa4838cbbcddc58e47e5b5820d36aa0
SifMk <130346848+SifMk@users.noreply.github.com>
2023-04-10 18:09:49 +0300

    Update print_temperature_without_1_4_7_question_6.cpp

--- print_temperature_without_1_4_7_question_6.cpp
+++ print_temperature_without_1_4_7_question_6.cpp
@@ -1,4 +1,5 @@
-/*print the closest temperature to the upper or lower with out the number 1,4 and 7*/
+/*print the closest temperature to the
+ upper or lower with out the number 1,4 and 7*/
 #include <iostream>
 #include <cmath>
 #include <string>
