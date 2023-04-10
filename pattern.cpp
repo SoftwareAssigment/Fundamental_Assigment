28dbebd836e7444b75d59da5a4c64c457a8afc14
Sefukamil20R <130300957+Sefukamil20R@users.noreply.github.com>
2023-04-10 16:01:47 +0200

    Create pattern.cpp

---
+++ pattern.cpp
@@ -0,0 +1,31 @@
+// create a dimond pattern.
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+    char n;
+    char *ptr;
+    cout<<"Enter any value you want expet e to print pattern: \n";
+    cin>>n;
+    if (n !='e'){
+    for(int i = 0; i < 15; i++){
+        for(int j = 0; j < 15; j++){
+            int num =  7 - abs(7 - j);
+            int num2 =  7 - abs(7 - i);
+            if (num + num2 <= 7){
+                ptr = new char (char(num+65));
+                cout<<*ptr<<" ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+        
+    }
+    }
+    else{
+    return 0;
+}  
+}
