7062434a5078f6581e2e1c1c6b8cba679d170078
SifMk <130346848+SifMk@users.noreply.github.com>
2023-04-11 00:44:47 -0700

    Update dynamic_pattern.cpp

--- without_function/dynamic_pattern.cpp
+++ without_function/dynamic_pattern.cpp
@@ -4,12 +4,12 @@
 #include <algorithm>
 using namespace std;
 int main() {
-    int n;
+    char n;
     char *ptr;
     cout<<"Enter any number you want: \n";
     cin>>n;
     
-    if (n >=1){
+    if (n >='1'){
         for(int i = 0; i < (n*2 +1); i++){
             for(int j = 0; j < (n*2 + 1); j++){
                 int num =  n - abs(n - j);
